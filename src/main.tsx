27cadb127f1804c79f31a6db27589a0130bb3ddf
Khaled <khaledesam384@gmail.com>
2025-10-07 11:57:39 +0300

    feat: google analytics manager tracking code added

--- src/main.tsx
+++ src/main.tsx
@@ -7,6 +7,7 @@ import { store, persistor } from './store';
 import { PersistGate } from 'redux-persist/integration/react';
 import { Provider as ReduxProvider } from 'react-redux';
 import { HeadProvider } from "react-head";
+import { Analytics } from "@/components/Analytics/Analytics";
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
@@ -14,6 +15,7 @@ createRoot(document.getElementById('root')!).render(
       <PersistGate loading={null} persistor={persistor}>
         <Provider>
           <HeadProvider>
+            <Analytics />
             <App />
           </HeadProvider>
         </Provider>
